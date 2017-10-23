ac17cead9850f0d2881d53d80b94c5ecb7dd3843
James Owen <james.owen@metaswitch.com>
2017-10-23 21:59:55 +0100

    Copy town endpoint documentation into view

--- townapi/api/views.py
+++ townapi/api/views.py
@@ -24,7 +24,49 @@ Views to create:
 
 
 class TownsView(generics.ListAPIView):
-    """ Standard list endpoint to return a list of all Town objects """
+    """
+        Simple endpoint to return a list of French towns and cities. For each
+        town, a JSON record is provided with information about the town and it
+        region, department and district. An example JSON record is as follows:
+
+            {
+                "town_code": "1",
+                "town_name": "L' Abergement-Clémenciat",
+                "population": 785,
+                "district_code": "2",
+                "department_code": "1",
+                "region_code": "84",
+                "region_name": "Auvergne-Rhône-Alpes"
+            }
+
+        Limit-offset pagination is provided, with the following syntax:
+
+            /towns?limit=<LIMIT>&offset=<OFFSET>
+
+        (`<LIMIT>` is set to 100 by default).
+
+        Ordering can be set using the syntax:
+
+            /towns?ordering=[-]<FIELD>
+
+        This will sort by the value of `<FIELD>` (all fields can be used
+        here). If the `-` is included, then the ordering will be in descending
+        order, and if it is omitted then it will be in ascending order.
+
+        Filtering can be done using the syntax:
+
+            /towns?<FILTER>=<VALUE>
+
+        Multiple filters can be chained using `&` characters. Available filters
+        are:
+
+        - Region Code (`region_code`)
+        - Department Code (`department_code`)
+        - District Code (`district_code`)
+        - Exact Population (`population`)
+        - Minimum Population (`min_population`)
+        - Maximum Population (`max_population`)
+    """
     queryset = Town.objects.all() \
                    .select_related('district',
                                    'district__department',
